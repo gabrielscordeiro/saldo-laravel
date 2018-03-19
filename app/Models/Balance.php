5853d1132bd783767d8c37bc694014398dd57bbb
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2018-03-19 16:08:48 -0300

    Adicionado beginTransaction na adição do Saldo

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Balance extends Model
 {
@@ -11,6 +12,8 @@ class Balance extends Model
 
     public function deposito(float $value): Array
     {
+        DB::beginTransaction();
+        
         $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.', '');
         $deposito = $this->save();
@@ -24,16 +27,22 @@ class Balance extends Model
         ]);
 
         if ($deposito && $historic) {
+            
+            DB::commit();
+            
             return [
                 'success' => true,
                 'message' => 'Registro inserido com sucesso'
             ];
+        }else{
+            DB::rollback();
+            
+            return [
+                'success' => false,
+                'message' => 'Falha ao tentar inserir o registro'
+            ];
         }
 
-        return [
-            'success' => false,
-            'message' => 'Falha ao tentar inserir o registro'
-        ];
     }
 
 }
